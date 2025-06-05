5102d77d3d88b6db36eb0634aa45654e56a32765
crjhappy123 <crjhappy@gmail.com>
2025-06-04 23:00:41 -0700

    Create stock_provider.py

---
+++ stock_provider.py
@@ -0,0 +1,14 @@
+# wechat_daily_bot/stock_provider.py
+import requests
+
+def get_stock_summary():
+    url = "http://hq.sinajs.cn/list=sh000001,sz399001,sz399006"
+    resp = requests.get(url)
+    lines = resp.text.split("\n")
+    index_map = {"sh000001": "上证指数", "sz399001": "深证成指", "sz399006": "创业板"}
+    summary = []
+    for line, code in zip(lines, index_map):
+        if '"' in line:
+            name, cur, open_, high, low = line.split('"')[1].split(',')[:5]
+            summary.append(f"{index_map[code]}：{cur}点，开盘{open_}，高{high}，低{low}")
+    return "\n".join(summary)
